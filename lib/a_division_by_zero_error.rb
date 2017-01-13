918810eece450e4b7ce94a0918db7f46ce6c38d3
Megan McLean <mmclean@protonmail.com>
2017-01-13 05:58:47 +0000

    Done.

--- lib/a_division_by_zero_error.rb
+++ lib/a_division_by_zero_error.rb
@@ -1,3 +1,5 @@
+# Learn.co Bootcamp Prep - Ruby Fundamentals - 03: Reading Error Messages
+
 42 / 6
 
 42 / 8
--- lib/a_type_error.rb
+++ lib/a_type_error.rb
@@ -1,3 +1,5 @@
+# Learn.co Bootcamp Prep - Ruby Fundamentals - 03: Reading Error Messages
+
 1 + 1
 
 1 + 2
