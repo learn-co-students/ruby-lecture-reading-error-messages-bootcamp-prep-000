194fd93f4390128f90d768aca1c67d1bc32e9bcb
Miles <miles.kv.taylor@gmail.com>
2018-09-27 10:29:12 +0000

    Done.

--- spec/no_ruby_errors_spec.rb
+++ spec/no_ruby_errors_spec.rb
@@ -3,7 +3,7 @@ describe "Not having any errors and being all green" do
   context 'NameError' do
     it 'raises a NameError when encountering undefined barewords' do
       expect{
-        load ''./lib/a_name_error.rb'
+        load './lib/a_name_error.rb'
       }.to_not raise_error
     end
   end
